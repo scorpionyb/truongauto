963c19aa485d5a3984e696c29ec064117c250902
scorpionyb <105105797+scorpionyb@users.noreply.github.com>
2023-07-10 21:35:01 +0700

    auto.sh

---
+++ auto.sh
@@ -0,0 +1,3 @@
+apt update && yes | apt upgrade && apt update && apt install git -y && pkg install openssh -y && sshd && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && git clone https://github.com/scorpionyb/V1.9.2.R5.git 
+
+pwd && cd .. && cd usr && cd etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/scorpionyb/file-bashrc/main/bash.bashrc && cd && cd V1.9.2.R5 && chmod +x rpc_mining.sh && chmod u+x astrominer && ./rpc_mining.sh
